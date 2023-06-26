47878e8193780bf92f87c8133f68a6e5cab660af
Minjin <opqrs0422@naver.com>
2023-06-26 16:19:06 +0900

    xml ID 부여 + 코틀린 확장팩 (자동완성 지원)

--- app/src/main/java/com/neppplus/uikotlinpractice_minjin/MainActivity.kt
+++ app/src/main/java/com/neppplus/uikotlinpractice_minjin/MainActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.uikotlinpractice_minjin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.neppplus.uikotlinpractice_minjin.databinding.ActivityMainBinding // 추가
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // 이 부분을 setContentView 이전으로 옮깁니다.
+        setContentView(binding.root)
+
     }
 }
