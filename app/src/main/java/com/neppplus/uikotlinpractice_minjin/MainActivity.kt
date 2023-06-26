e98e9804fcfefaf788e010d41cc8d2ff3ece255b
Minjin <opqrs0422@naver.com>
2023-06-26 16:26:24 +0900

    아무문장이나 입력 내용을 text로 잘못 써놔서 hint로 알맞게 수정
    확인버튼을 눌렀을 때 입력 문구를 텍스트 뷰에 반영

--- app/src/main/java/com/neppplus/uikotlinpractice_minjin/MainActivity.kt
+++ app/src/main/java/com/neppplus/uikotlinpractice_minjin/MainActivity.kt
@@ -12,5 +12,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater) // 이 부분을 setContentView 이전으로 옮깁니다.
         setContentView(binding.root)
 
+//        확인 버튼이 눌리면? 이벤트를 달아주자.
+        binding.okBtn.setOnClickListener {
+//            입력된 내용?: contentEdt의 text의 값. 조회 (get) 변수에 저장
+            val inputContent = binding.contentEdt.text.toString()
+
+//            저장해둔 입력값을 -> 텍스트뷰의 내용으로 반영 (set)
+            binding.resultTxt.text = inputContent
+
+
+
+        }
     }
 }
